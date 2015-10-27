fa82b553945b08ab168388bcbcc5cba3634eaf70
Saevar Berg <saevar1990@hotmail.com>
2015-10-27 15:17:50 +0100

    Added !rejoin command.

--- src/MonadBot/Plugins/Join.hs
+++ src/MonadBot/Plugins/Join.hs
@@ -4,15 +4,49 @@ module MonadBot.Plugins.Join
     ) where
 
 import Control.Monad
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Monoid
+import System.Random
 
 import MonadBot.Types
+
+
+messages :: [Text]
+messages =
+    [ "Carrier has arrived."
+    , "Warp fields stabilized."
+    , "It's a good day to die."
+    , "We are as one."
+    , "Adun Toridas."
+    , "I have returned."
+    , "Teleport successful."
+    , "My life for Aiur!"
+    , "Monadbot operational."
+    , "Can I take your order?"
+    , "Somebody call for an exterminator?"
+    , "Monadbot online."
+    , "SCV good to go, sir."
+    ]
+
+joinChannel channel = do
+    choice <- liftIO $ randomRIO (0, m)
+    sendCommand "JOIN" [channel]
+    sendPrivmsg channel [messages !! choice]
+  where
+    m = length messages - 1
 
 joinHandler :: SimpleHandler
 joinHandler _ = handles "376" $ do
     srv <- getServer
     logMsg "Joining channels"
-    forM_ (serverChannels srv) $ \channel ->
-        sendCommand "JOIN" [channel]
+    mapM_ joinChannel (serverChannels srv)
+
+rejoin :: SimpleHandler
+rejoin _ = handleBang "!rejoin" $ do
+    (chan:_) <- getParams
+    sendCommand "PART" [chan]
+    joinChannel chan
 
 plugin :: Plugin
-plugin = mkSimplePlugin "Join handler" [joinHandler]
+plugin = mkSimplePlugin "Join handler" [joinHandler, rejoin]
