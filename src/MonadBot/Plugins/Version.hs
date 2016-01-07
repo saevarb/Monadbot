c3c72b92d59db06713aff22c7b28990407ab3920
Sævar Berg <icedane@gmail.com>
2016-01-07 23:52:58 +0100

    Fleshed out version plugin a bit + uptime.

--- src/MonadBot/Plugins/Version.hs
+++ src/MonadBot/Plugins/Version.hs
@@ -3,17 +3,18 @@ module MonadBot.Plugins.Version
     ( plugin
     ) where
 
-import Paths_monadbot (version)
-import Data.Version (showVersion)
+import Data.List
 import Data.Text (pack)
-import Data.Time (getCurrentTime)
-
+import Data.Version (showVersion)
+import Paths_monadbot (version)
+import Data.Time
+import Data.Time.Clock
 
 import MonadBot.Message
 import MonadBot.Plugin.Development
 
 
-versionHandler :: SimpleHandler
+versionHandler :: PluginM UTCTime ()
 versionHandler = onCtcp "VERSION" $ do
     pref <- getPrefix
     case pref of
@@ -21,18 +22,43 @@ versionHandler = onCtcp "VERSION" $ do
             ctcpReply p ["VERSION", "monadbot v" <> pack (showVersion version)]
         _ -> return ()
 
-userVersion :: SimpleHandler
+userVersion :: PluginM UTCTime ()
 userVersion = onUserCmds ["$version", "$info"] $ do
-    return ()
+    (channel:_) <- getParams
+    sendPrivmsg channel ["I am monadbot v" <> pack (showVersion version)]
+    start <- readState
+    current <- liftIO getCurrentTime
+    let diff = round $ diffUTCTime current start
+    let formatted = formatSeconds diff
+    sendPrivmsg channel ["Uptime:", pack formatted]
 
-initState :: PluginM UTCTime UTCTime
+formatSeconds :: Integer -> String
+formatSeconds s = unwords $ formatSeconds' s units
+  where
+   formatSeconds' _ [] = []
+   formatSeconds' remainder ((label, time):us) =
+       case divMod remainder time of
+           (0, r) ->
+               formatSeconds' r us
+           (n, r) ->
+               (show n ++ label) : formatSeconds' r us
+   units =
+       [ ("y", 365 * 24 * 60 * 60)
+       , ("m", 30 * 24 * 60 * 60)
+       , ("d", 24 * 60 * 60)
+       , ("h", 60 * 60)
+       , ("m", 60)
+       , ("s", 1)
+       ]
+
+initState :: Irc UTCTime
 initState =
     liftIO getCurrentTime
 
-plugin :: Plugin ()
+plugin :: Plugin UTCTime
 plugin =
     Plugin
     "Version and info handler"
-    [versionHandler]
+    [versionHandler, userVersion]
     initState
     (const $ return ())
