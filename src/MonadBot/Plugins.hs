ed6ee0eec4e3147b72594667b39d4ed4e1afab66
Sævar Berg <icedane@gmail.com>
2016-01-07 05:53:04 +0100

    Removed old plugin.

--- src/MonadBot/Plugins.hs
+++ src/MonadBot/Plugins.hs
@@ -7,9 +7,7 @@ import qualified MonadBot.Plugins.Ping as Ping
 import qualified MonadBot.Plugins.Join as Join
 import qualified MonadBot.Plugins.Test as Test
 import qualified MonadBot.Plugins.Version as Version
-import qualified MonadBot.Plugins.Nixers as Nixers
 import qualified MonadBot.Plugins.Auth as Auth
-import qualified MonadBot.Plugins.Url as Url
 import qualified MonadBot.Plugins.Brainfuck as Brainfuck
 
 allPlugins :: [Hide Plugin]
@@ -18,8 +16,6 @@ allPlugins =
     , Enabled Join.plugin
     , Enabled Test.plugin
     , Enabled Version.plugin
-    , Enabled Nixers.plugin
     , Enabled Auth.plugin
-    , Disabled Url.plugin
     , Enabled Brainfuck.plugin
     ]
