1fa672a080e7a440faa20ae1b617db9df5763b6f
Sævar Berg <icedane@gmail.com>
2016-03-18 00:52:23 +0100

    chore(refactor): Refactored code into library
    - Refactored all code into
      - app/ for the executable
      - lib/ for all the real code
      - created /test for tests
    - Removed old Writer.hs module
    - Modified cabal file accordingly

---
+++ lib/MonadBot/DB.hs
@@ -0,0 +1,10 @@
+module MonadBot.DB
+    ( module Database.Persist
+    , module Database.Persist.Sql
+    , module Database.Persist.Sqlite
+    )
+    where
+
+import Database.Persist
+import Database.Persist.Sql
+import Database.Persist.Sqlite
